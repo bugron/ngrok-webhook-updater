3bb22f2ed970e2e5837da026baf69b92bb54aafc
Arsen Melikyan <bugron@mail.ru>
2019-01-06 22:26:40 +0400

    chore(eslint): introduce ESLint for source code linting

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: 'airbnb-base',
+  rules: {
+    'linebreak-style': 'off',
+    'no-console': 'off',
+    'comma-dangle': 'off',
+    'consistent-return': 'off',
+  }
+};
